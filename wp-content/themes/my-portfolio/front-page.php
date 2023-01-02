45fb545b65a9ea8f8b6a989cc4a1ddc531333007
jhalak dhami <jhalaksime@gmail.com>
2023-01-02 21:33:43 +0545

    portfolio evening

--- wp-content/themes/my-portfolio/front-page.php
+++ wp-content/themes/my-portfolio/front-page.php
@@ -3,11 +3,11 @@
     <div class="page py-3">
         <div class="row">
             <div class="col-md-5 py-5 px-2">
-                <h1><?php bloginfo()?></h1>
-                <p><?php the_field('post')?></p>
+                <h1 class="px-2 py-3 mt-5"><?php bloginfo()?></h1>
+                <p class="py-2"><?php the_field('post',16)?></p>
             </div>
             <div class="col-md-7">
-                <img src="<?php the_field('front_page_image') ?>" class="rounded-5" height="auto" width="100%">
+                <img src="<?php the_field('front_page_image') ?>" class="rounded-circle" height="100%" width="90%">
             </div>
         </div>
 
